5b2f986101c6aeae2513825be222947ba5edec89
ManuelDev <manueldeveloper17@gmail.com>
2022-12-16 22:17:16 -0500

    Add config env

--- config.js
+++ config.js
@@ -6,4 +6,14 @@ module.exports = {
   jwt: {
     secret: process.env.JWT_SECRET || "keySecret",
   },
+  mysql: {
+    host: process.env.MYSQL_HOST || "localhost",
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "admin123",
+    database: process.env.MYSQL_DB || "db_redSocial",
+  },
+  mysqlServide: {
+    host: process.env.MYSQL_SRV_HOST || "localhost",
+    port: process.env.MYSQL_SRV_PORT || 3001,
+  },
 };
