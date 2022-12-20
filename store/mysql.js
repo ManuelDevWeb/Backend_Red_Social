111af5ffbaef12a4a980fb9c568cbb0b183c0fb8
ManuelDev <manueldeveloper17@gmail.com>
2022-12-20 02:41:03 -0500

    Add mysql functions and queries

--- store/mysql.js
+++ store/mysql.js
@@ -1,7 +1,10 @@
 // Importando mysql
 const mysql = require("mysql2");
+
 // Importando variables de entorno
 const config = require("../config");
+// Funcion para personalizar y manejar errores
+const error = require("../utils/error");
 
 // Configuracion DB
 const dbConfig = {
@@ -14,7 +17,7 @@ const dbConfig = {
 // Conexion
 let connection;
 
-// Funcion para conectarnos a la DB
+// Conectarnos a la DB
 function handleConnection() {
   // Creando la conexion a la DB de mysql
   connection = mysql.createConnection(dbConfig);
@@ -39,5 +42,85 @@ function handleConnection() {
     }
   });
 }
+
+// Listar los datos de una tabla
+function list(table) {
+  // Una promesa es el exito o el fracaso de una operacion asincrona
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM ${table}`, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+    });
+  });
+}
+
+// Obtener dato de una tabla
+async function get(table, id) {
+  // Consultando el dato a traves de la query
+  const element = await connection.query(
+    `SELECT * FROM ${table} WHERE id=${id}`
+  );
+
+  if (!element || element === null) {
+    throw error("User doesn't exist", 500);
+  }
+
+  return element;
+}
+
+// Insertar dato en una tabla
+async function insert(table, data) {
+  // Insertamos el dato a traves de la query
+  await connection.query(
+    `
+    INSERT INTO ${table} SET ?
+  `,
+    // ? son los valores que se van a setear al momento de recibir data
+    data
+  );
+
+  return true;
+}
+
+// Actualizar dato de una tabla
+async function update(table, id, data) {
+  // Actualizamos el elemento a traves de la query
+  await connection.query(
+    `
+    UPDATE ${table} SET ? WHERE id=?`,
+    // El primer ? son los valores que se van a setear con la data recibida, el segundo ? es el valor del id que se recibe
+    [data, id]
+  );
+
+  return true;
+}
+
+// Eliminar dato de una tabla
+async function remove(table, id) {
+  // Eliminamos el elemento a traves de la query
+  await connection.query(
+    `
+    DELETE FROM ${table} WHERE id=?
+  `,
+    // ? es el valor del id que se recibe
+    id
+  );
+
+  return true;
+}
+
+// Obtener dato por atributos diferentes a id
+function query() {}
+
+// Exportando las funciones
+module.exports = {
+  list,
+  get,
+  insert,
+  update,
+  remove,
+  query,
+};
 
 handleConnection();
