cb3a37fd50d5990e21a8d243257feac9951c2168
ManuelDev <manueldeveloper17@gmail.com>
2022-12-29 01:41:13 -0500

    Finishied microservice mysql

--- store/remote-mysql.js
+++ store/remote-mysql.js
@@ -1,4 +1,4 @@
-// Aca en las peticiones llamamos la API del microservicio de mysql
+// Este storage hace llamados a la API del microservicio de mysql que apunta al store mysql
 
 // Importando clase instancia de Axios
 const AxiosInstance = require("./../utils/axios-instance");
@@ -16,8 +16,29 @@ const axiosRequest = new AxiosInstance({
 
 // A diferencia de antes las peticiones ya se hacen a otra API que llama los metodos del store mysql y no directamente al archivo mysql
 
+// Funcion para obtener listado de datos de una tabla, hace peticion a otra API (Microservicio)
 async function list(table) {
   return await axiosRequest.send(`/${table}`, "GET");
 }
 
-module.exports = { list };
+// Funcion para obtener un dato de una tabla, hace peticion a otra API (Microservicio)
+async function get(table, id) {
+  return await axiosRequest.send(`/${table}/${id}`, "GET");
+}
+
+// Funcion para insertar datos de una tabla, hace peticion a otra API (Microservicio)
+async function insert(table, data) {
+  return await axiosRequest.send(`/${table}`, "POST", data);
+}
+
+// Funcion para actualizar datos de una tabla, hace peticion a otra API (Microservicio)
+async function update(table, id, data) {
+  return await axiosRequest.send(`/${table}/${id}`, "PATCH", data);
+}
+
+// Funcion para eliminar datos de una tabla, hace peticion a otra API (Microservicio)
+async function remove(table, id) {
+  return await axiosRequest.send(`/${table}/${id}`, "DELETE");
+}
+
+module.exports = { list, get, insert, update, remove };
